149d4cb569425e8b52b5da50138a124c89ed811c
insprac <insprac.dev@gmail.com>
2026-01-16 19:54:33 +1300

    Add quality clamping following logic removing excessive conditionals

--- src/gildedrose.rs
+++ src/gildedrose.rs
@@ -43,25 +43,17 @@ impl GildedRose {
             }
 
             if item.name != AGED_BRIE && item.name != BACKSTAGE_PASSES {
-                if item.quality > 0 {
-                    item.quality -= 1;
-                }
+                item.quality -= 1;
             } else {
-                if item.quality < 50 {
-                    item.quality += 1;
+                item.quality += 1;
 
-                    if item.name == BACKSTAGE_PASSES {
-                        if item.sell_in < 11 {
-                            if item.quality < 50 {
-                                item.quality += 1;
-                            }
-                        }
+                if item.name == BACKSTAGE_PASSES {
+                    if item.sell_in < 11 {
+                        item.quality += 1;
+                    }
 
-                        if item.sell_in < 6 {
-                            if item.quality < 50 {
-                                item.quality += 1;
-                            }
-                        }
+                    if item.sell_in < 6 {
+                        item.quality += 1;
                     }
                 }
             }
@@ -69,22 +61,17 @@ impl GildedRose {
             if item.sell_in <= 0 {
                 if item.name != AGED_BRIE {
                     if item.name != BACKSTAGE_PASSES {
-                        if item.quality > 0 {
-                            if item.name != SULFURAS {
-                                item.quality -= 1;
-                            }
-                        }
+                        item.quality -= 1;
                     } else {
                         item.quality = 0;
                     }
                 } else {
-                    if item.quality < 50 {
-                        item.quality += 1;
-                    }
+                    item.quality += 1;
                 }
             }
 
             item.sell_in -= 1;
+            item.quality = item.quality.clamp(0, 50);
         }
     }
 }
